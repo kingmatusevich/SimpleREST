d01ffd434c768561729e5b273b92a2c47f676b81
kingmatusevich <javiermatusevich@gmail.com>
2012-12-07 17:10:27 -0200

    Update API/index.php

--- API/index.php
+++ API/index.php
@@ -5,11 +5,11 @@ include_once('rest.api.php');		// This includes rest.api, rest.parser, error.deb
 $globalDebugEnabled = false;		// Debugging of global errors in error.debug, default behaviour if not specified is set to true
 
 $API = new RESTAPI(true);			
-									/////////////////////////////////////////////////////////////////////////////////////////////////	
-									/* instantiation of rest.api with internal debugging set to true, this also sets the debugging of 
-									the instance of rest.parser in rest.api to true. Default behaviour is false. Default behaviour of
-									rest.parser if it were not set by rest.api is set to false. */
-									/////////////////////////////////////////////////////////////////////////////////////////////////
+					/////////////////////////////////////////////////////////////////////////////////////////////////	
+					/* instantiation of rest.api with internal debugging set to true, this also sets the debugging of 
+					the instance of rest.parser in rest.api to true. Default behaviour is false. Default behaviour of
+					rest.parser if it were not set by rest.api is set to false. */
+					/////////////////////////////////////////////////////////////////////////////////////////////////
 
-$API->start();						//initiates the parsing and processing process inside the instantiated rest.api
+$API->start();	//initiates the parsing and processing process inside the instantiated rest.api
 ?>
