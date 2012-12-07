1655d31d57212531d4d575b44ac3c360aad30e18
Javier Matusevich <javiermatusevich@gmail.com>
2012-12-07 13:05:57 -0300

    more documentation

--- API/index.php
+++ API/index.php
@@ -5,7 +5,8 @@ include_once('rest.parser.php');	// Already included inside rest.api, included f
 include_once('rest.api.php');		// This includes rest.api, rest.parser, error.debug	
 $globalDebugEnabled = false;		// Debugging of global errors in error.debug, default behaviour if not specified is set to true
 
-$API = new RESTAPI(true);		/////////////////////////////////////////////////////////////////////////////////////////////////	
+$API = new RESTAPI(true);		
+					/////////////////////////////////////////////////////////////////////////////////////////////////	
 					/* instantiation of rest.api with internal debugging set to true, this also sets the debugging of 
 					the instance of rest.parser in rest.api to true. Default behaviour is false. Default behaviour of
 					rest.parser if it were not set by rest.api is set to false. */
