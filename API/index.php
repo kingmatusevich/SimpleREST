9b42b46528079f772dd4c9b04155e39c056a5444
Javier Matusevich <javiermatusevich@gmail.com>
2012-12-07 11:36:32 -0300

    debug method, working parser of parameters

--- API/index.php
+++ API/index.php
@@ -1,7 +1,7 @@
 <?php
 class Error
 {
-	private static $errors = array(0 => 'Invalid or null query',1 => 'Undefined');
+	protected static $errors = array(0 => 'Invalid query',1 => 'Null query', 2 => 'Invalid Error');
 	
 	public static function send($i)
 	{
@@ -11,7 +11,7 @@ class Error
 		}
 		else
 		{
-			
+			echo self::$errors[2];
 		}
 	}
 	
@@ -19,12 +19,15 @@ class Error
 }
 class RESTParser
 {
-	private $queryString;
-	private $remainingQueryString;
-	
+	protected $queryString;
+	protected $components;
+	protected $current;
 	public function __construct($query)
 	{
 		$this->queryString = $query;
+		echo $query.'</br>';
+		$this->components = array_filter(explode('/', $this->queryString));
+		$this->current = 1;
 		//echo $this->queryString; // to be deleted, debug
 	}
 	public function getQueryString()
@@ -34,18 +37,30 @@ class RESTParser
 	public function nextBlock()
 	{
 		// return current block and trim it from remaining query string
+		$tmp = $this->components[$this->current];
+		$this->current++;
+		return $tmp;
+		
 	}
 	public function isEmpty()
 	{
 		// Do logic here
-		return true; // or return false
+		$comp = count($this->components);
+		$curr = $this->current;
+
+		if ($curr <= $comp)
+		{
+			return false;
+		} else return true;
+		
+
 	}
 }
 class RESTAPI
 {
-	private $queryString;
-	private $RESTParser;
-	private $currentBlock;
+	protected $queryString;
+	protected $RESTParser;
+	protected $currentBlock;
 	
 	public function __construct()
 	{
@@ -57,27 +72,39 @@ class RESTAPI
 		if (!$this->RESTParser->isEmpty())
 		{
 			$this->currentBlock = $this->RESTParser->nextBlock();
+			//echo '</br>'.$this->currentBlock.'</br>';
 			switch($this->currentBlock)
 			{
 				case 'API':
-				$this->start; break;
+				$this->start(); break;
 				case 'auth':
-				$this->authentification; break;
+				$this->authentification(); break;
 				case 'news':
-				$this->news; break;
+				$this->news(); break;
+				case 'debug':
+				$this->debug(); break;
 				default:
 				Error::send(0);
 			}
 		} else
 		{
-			Error::send(0);
+			Error::send(1);
 		}		
 	}
-	public function authentification()
+	protected function debug()
+	{
+		echo 'debug </br>';
+		while (!$this->RESTParser->isEmpty())
+		{
+			echo $this->RESTParser->nextBlock().'</br>';
+		}
+	}
+	protected function authentification()
 	{
 		echo 'authentification'; // for debug only
+
 	}
-	public function news()
+	protected function news()
 	{
 		echo 'news';  // for debug only
 	}
