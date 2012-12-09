40fde4055f1ef061db64f35a3b54d66f34d17edf
Javier Matusevich <javiermatusevich@gmail.com>
2012-12-09 17:26:54 -0300

    Completed transition to new subclass method of REST API

--- API/index.php
+++ API/index.php
@@ -3,8 +3,40 @@ include_once('error.debug.php');	// Already included inside rest.api, included f
 include_once('rest.parser.php');	// Already included inside rest.api, included for documentation
 include_once('rest.api.php');		// This includes rest.api, rest.parser, error.debug	
 $globalDebugEnabled = true;		// Debugging of global errors in error.debug, default behaviour if not specified is set to true
+////////////////////////////////////////////////////////////////////////
+//SHARED OBJECTS
+$queryString = $_SERVER['QUERY_STRING'];
+$RESTParser = new RESTParser($queryString, true);
 
-$API = new RESTAPI(false);			
+////////////////////////////////////////////////////////////////////////
+//DEBUG
+$debug = new DebugAction(array(), 'debug');
+
+////////////////////////////////////////////////////////////////////////
+//USERS GET
+$usersGET = new UsersGETAction(array(), 'GET');
+
+//USERS
+$usersList = array($usersGET);
+$users = new RESTObject($usersList, 'users', $RESTParser);
+
+////////////////////////////////////////////////////////////////////////
+//AUTH SESSION GET
+$authSessionGET = new SessionGETAction(array(), 'GET');
+
+//AUTH SESSION
+$authSessionList = array($authSessionGET
+$authSession = new RESTObject($authSessionList, 'session', $RESTParser);
+
+//AUTH
+$authList = array($authSession);
+$auth = new RESTObject($authList, 'auth', $RESTParser);
+
+////////////////////////////////////////////////////////////////////////
+//API
+$APIList = array($auth, $users, $debug);
+$API = new RESTAPI($APIList, false, $RESTParser);			
+
 					/////////////////////////////////////////////////////////////////////////////////////////////////	
 					/* instantiation of rest.api with internal debugging set to true, this also sets the debugging of 
 					the instance of rest.parser in rest.api to true. Default behaviour is false. Default behaviour of
--- API/rest.actions.php
+++ API/rest.actions.php
@@ -12,14 +12,50 @@ class DebugAction extends RESTAction()
 {
 	public function start()
 	{
-		protected function debug()
-		{
 			echo 'debug </br>';
 			while (!$this->RESTParser->isEmpty())
 			{
 				echo $this->RESTParser->nextBlock().'</br>';
 			}
+	}
+}
+
+class SessionGETAction extends RESTAction()
+{
+	public function start()
+	{
+		$user = $_POST['user'];
+		$password = $_POST['password'];
+		if ($user && $password)
+		{
+			$session = $this->dataManager->users->attemptLogin($user, $password);
+			if ($session)
+			{
+				header('Content-Type: application/json');
+				echo json_encode(array('session' => $session));
+			} else
+			{
+				header('WWW-Authenticate: Negotiate');
+				Error::send(3);
+			}
+		} else
+		{
+			header('WWW-Authenticate: Negotiate');
+			Error::send(3);
 		}
+	}
+}
+
+class UsersGETAction extends RESTAction()
+{
+	public function start()
+	{
+		$users = $this->dataManager->users->allItems();
+		if ($users)
+		{
+			header('Content-Type: application/json');
+			echo json_encode($users);
+		} else Error::send(DatabaseError);
 	}
 }
 ?>
